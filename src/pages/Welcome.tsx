54275f31ec01ed3446c7790de3bd4d337e41d32c
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-04-20 19:48:46 -0300

    feat: add fonts

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,40 +1,43 @@
 import React from 'react';
-import { SafeAreaView, Text, Image, StyleSheet, TouchableOpacity, Dimensions} from 'react-native';
+import { SafeAreaView, Text, Image, StyleSheet, TouchableOpacity, Dimensions, View} from 'react-native';
 import { Feather } from '@expo/vector-icons'
 
 import wateringImg from '../assets/watering.png'
 import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
 
 export function Welcome() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas {'\n'}
-        de forma fácil
-      </Text>
-      
-      <Image 
-        source={wateringImg} 
-        style={styles.image}
-        resizeMode='contain'
-      />
-  
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
-      <TouchableOpacity 
-        style={styles.button}
-        activeOpacity={0.7}
-      >
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de{'\n'}
+          forma fácil
+        </Text>
         
-        <Feather 
-          name='chevron-right' 
-          style={styles.buttonIcon}
+        <Image 
+          source={wateringImg} 
+          style={styles.image}
+          resizeMode='contain'
         />
-      </TouchableOpacity>
+    
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
+        <TouchableOpacity 
+          style={styles.button}
+          activeOpacity={0.7}
+        >
+          
+          <Feather 
+            name='chevron-right' 
+            style={styles.buttonIcon}
+          />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -42,21 +45,28 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
+    paddingHorizontal: 20,
   },
   title: {
     marginTop: 38,
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign:'center',
     color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
   subtitle: {
     fontSize: 18,
     textAlign:'center',
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
   },
   image: {
     height: Dimensions.get('window').width * 0.7,
