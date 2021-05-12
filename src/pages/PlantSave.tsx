b4b66f776e0c721d1074f0813325eaa28b97dcf7
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-05-12 19:23:45 -0300

    feat: create date time picker

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -1,29 +1,66 @@
-import React from 'react';
-import { SafeAreaView, Text, StyleSheet, View, Image, ScrollView, Platform, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, StyleSheet, View, Image, ScrollView, Platform, TouchableOpacity, Alert} from 'react-native';
 import { SvgFromUri } from 'react-native-svg';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { useRoute } from '@react-navigation/core';
+import DateTimePicker, { Event } from '@react-native-community/datetimepicker';
+import { isBefore, format} from 'date-fns';
 
 import waterDrop from '../assets/waterdrop.png'
 import { Button } from '../components/Button';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
-
+import { platform } from 'os';
+interface Params {
+  id: string;
+  name: string;
+  about: string;
+  water_tips: string;
+  photo: string;
+  environments: [string],
+  frequency: {
+    times: number,
+    repeat_every: string;
+  }
+}
 export function PlantSave() { 
+  const [selectedDateTime, setSelectedDateTime] = useState(new Date ());  
+  const [showDatePicker, setShowDatePicker] = useState(Platform.OS === 'ios');   
+
+  const route = useRoute();
+  const plant = route.params as Params ;
+
+  function handleChangeTime(event: Event, dateTime: Date | undefined) {
+    if (Platform.OS === 'android') {
+      setShowDatePicker(oldState => !oldState);
+    }
+    if (dateTime && isBefore(dateTime, new Date ())) {
+      setSelectedDateTime(new Date ());
+      return Alert.alert('Escolha uma hora no futuro ! ⏰ ')
+    }
+
+    if (dateTime) 
+      setSelectedDateTime(dateTime);
+  }
+
+  function handleOpenDateTimePickerFromAndroid() {
+    setShowDatePicker(oldState => !oldState);
+  }
+
+
   return (
     <View style={styles.container}>
       <View style={styles.plantInfo}>
         <SvgFromUri
-          uri=''
+          uri={plant.photo}
           height={150}
           width={150}
         />
         <Text style={styles.plantName}>
-          Nome da Planta
+          {plant.name}
         </Text>
         <Text style={styles.plantAbout}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nisi 
-          delectus blanditiis. Iure maiores quas nam consequatur dolor aspernatur. 
-          Est ipsam aut eius facere expedita? At culpa molestias eos maxime!
+          {plant.about}
         </Text>
       </View>
       <View style={styles.controller}>
@@ -33,12 +70,35 @@ export function PlantSave() {
               style={styles.tipImage}
             />
             <Text style={styles.tipText}>
-              Lorem ipsum dolor sit, Lorem ipsum dolor sit, 
+              {plant.water_tips}
             </Text>
           </View>
           <Text style={styles.alertLabel}>
             Escolha o melhor horário para ser lembrado:
           </Text>
+
+          {showDatePicker && (
+            <DateTimePicker
+            value={selectedDateTime}
+            mode='time'
+            display='spinner'
+            onChange={handleChangeTime}
+          />
+          )}
+
+          {
+            Platform.OS === 'android' && (
+              <TouchableOpacity
+                style={styles.dateTimePickerButton}
+                onPress={handleOpenDateTimePickerFromAndroid}
+              >
+
+                <Text style={styles.dateTimePickerText}>
+                  {`Mudar ${format(selectedDateTime, 'HH:mm')}`}
+                </Text>
+              </TouchableOpacity>
+            )
+          }
 
           <Button 
             title='Cadastrar planta'
@@ -110,6 +170,16 @@ const styles = StyleSheet.create({
     color: colors.heading,
     fontSize: 12,
     marginBottom: 5,
+  },
+  dateTimePickerButton: {
+    width: '100%',
+    alignItems: 'center',
+    paddingVertical: 40,
+  },
+  dateTimePickerText: {
+    color: colors.heading,
+    fontSize: 24,
+    fontFamily: fonts.text,
   }
 
 })
